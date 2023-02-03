996a0b82906909fba26f3bdd4727e2a7c705d206
moyasi <isikamen3711@gmail.com>
2023-02-03 22:04:42 +0900

    230203 モジュール化

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@
   const title = document.querySelector(".main_title");
 
   const history_log = document.querySelector(".history_log");
-  let challenge_time = 0;
 
   const start_button = document.querySelector(".start_button");
   const input_button = document.querySelector(".input_button");
@@ -16,10 +15,11 @@
   let truth_10 = 0;
   let truth_1 = 0;
   let truth_number = 0;
+  let challenge_time = 0;
 
 
   //全角数字を半角に変換
-  function zen_to_han(str) {
+  function replace_narrow(str) {
     return str.replace(/[０-９]/g, function (s) {
       return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
     });
@@ -28,7 +28,7 @@
   //解答の生成
   function generate_numbers() {
     const numbers = [...Array(10).keys()]
-  
+
     for (let i = numbers.length - 1; i >= 0; i--) {
       let x = Math.floor(Math.random() * i);
       [numbers[i], numbers[x]] = [numbers[x], numbers[i]]
@@ -38,13 +38,11 @@
     truth_1 = numbers[2];
 
     truth_number = (truth_100 * 100 + truth_10 * 10 + truth_1);
-    //
-  
-    // console.log(`答えは ${truth_number}`);
+    // console.log(truth_number);
   }
 
-  start_button.addEventListener('click', () => {
-    generate_numbers()
+  //盤面リセット
+  function game_reset() {
     start_area.classList.add("invisible");
     start_button.classList.remove("restart");
     start_button.textContent = "スタート";
@@ -54,6 +52,7 @@
     title.textContent = "Hit & Blow";
     challenge_time = 0;
 
+    //履歴のリセット
     for (let n = 1; n <= 10; n++) {
       // console.log("run")
       history_log.children[n].children[1].textContent = "";
@@ -61,114 +60,108 @@
       history_log.children[n].children[3].textContent = "";
       answer.value = "";
     }
+  }
 
+  //ヒットチェック
+  function hit_check(n) {
+    let a_100 = Math.floor(n / 100 % 10)
+    let a_10 = Math.floor(n / 10 % 10)
+    let a_1 = Math.floor(n / 1 % 10)
+    let hit_count = 0;
+
+    if (truth_1 == a_1) {
+      hit_count++;
+    }
+    if (truth_10 == a_10) {
+      hit_count++;
+    }
+    if (truth_100 == a_100) {
+      hit_count++;
+    }
+    return hit_count;
+  }
+
+  //ブローチェック
+  function blow_check(n) {
+    let a_100 = Math.floor(n / 100 % 10)
+    let a_10 = Math.floor(n / 10 % 10)
+    let a_1 = Math.floor(n / 1 % 10)
+    let blow_count = 0;
+
+    let t_n = [truth_100, truth_10, truth_1]
+
+    if (t_n.indexOf(a_1) !== -1) {
+      blow_count++;
+    }
+    if (t_n.indexOf(a_10) !== -1) {
+      blow_count++;
+    }
+    if (t_n.indexOf(a_100) !== -1) {
+      blow_count++;
+    }
+    return blow_count;
+  }
+
+  //履歴の記録
+  function enter_record(a, b, c) {
+    challenge_time++;
+    history_log.children[challenge_time].children[1].textContent = a;
+    history_log.children[challenge_time].children[2].textContent = b;
+    history_log.children[challenge_time].children[3].textContent = c;
+  }
+
+  //開始ボタン
+  start_button.addEventListener('click', () => {
+    generate_numbers()
+    game_reset()
     message_area.innerHTML = "3ケタの数字を　入力してください";
   });
 
+  //回答ボタン
   input_button.addEventListener('click', () => {
 
     message_area.classList.remove("warn");
-    let answer_nm = zen_to_han(answer.value);//全角数字を半角に変換
-    console.log(answer_nm);
+    let answer_nm = replace_narrow(answer.value);//全角数字を半角に変換
 
     if (/^\d{3}$/.test(answer_nm)) {
-      //３桁の数字だった場合
-      // console.log("correct");
-      let hit_count = 0;
-      let blow_count = 0;
 
-      let answer_100 = Math.floor(answer_nm / 100 % 10)
-      let answer_10 = Math.floor(answer_nm / 10 % 10)
-      let answer_1 = Math.floor(answer_nm / 1 % 10)
+      let hit_count = hit_check(answer_nm);
+      let blow_count = blow_check(answer_nm);
 
-      // console.log(`1の位は ${answer_1}`);
-      // console.log(`10の位は ${answer_10}`);
-      // console.log(`100の位は ${answer_100}`);
+      blow_count = blow_count - hit_count;
+      if (blow_count < 0) {
+        blow_count = 0
+      }
 
-      {
-        {
-          //ヒット数チェック
-          if (truth_1 == answer_1) {
-            // console.log("1の位ヒット");
-            hit_count++;
-          }
-          if (truth_10 == answer_10) {
-            // console.log("10の位ヒット");
-            hit_count++;
-          }
-          if (truth_100 == answer_100) {
-            // console.log("100の位ヒット");
-            hit_count++;
-          }
-        }
+      enter_record(answer_nm,hit_count,blow_count);
 
-        {
-          //ブロー数チェック
-          let t_numbers = [truth_100, truth_10, truth_1]
-          console.log(t_numbers);
+      message_area.innerHTML = `ヒット数は ${hit_count}<br>ブロー数は ${blow_count}です`
 
-          if (t_numbers.indexOf(answer_1) !== -1) {
-            blow_count++;
-            // console.log("回答1の位ブロー");
-          }
-          if (t_numbers.indexOf(answer_10) !== -1) {
-            // console.log("回答10の位ブロー");
-            blow_count++;
-          }
-          if (t_numbers.indexOf(answer_100) !== -1) {
-            // console.log("回答100の位ブロー");
-            blow_count++;
-          }
-          blow_count = blow_count - hit_count;
-          if (blow_count < 0) {
-            blow_count = 0
-          }
-        }
-
-        // console.log(`ヒット数は ${hit_count}`);
-        // console.log(`ブロー数は ${blow_count}`);
-
-        {
-          challenge_time++;
-          history_log.children[challenge_time].children[1].textContent = answer_nm;
-          history_log.children[challenge_time].children[2].textContent = hit_count;
-          history_log.children[challenge_time].children[3].textContent = blow_count;
-
-          message_area.innerHTML = `ヒット数は ${hit_count}<br>ブロー数は ${blow_count}です`
-        }
-        if (hit_count == 3) {
-          message_area.innerHTML = `お見事です<br>
+      //判定結果
+      if (hit_count == 3) {
+        message_area.innerHTML = `お見事です<br>
           正解は ${truth_number} でした`
-          input_area.classList.add("invisible");
-          start_area.classList.remove("invisible")
-          start_button.textContent = "もう一度遊ぶ";
-        } else if (challenge_time === 10) {
-          title.classList.add("warn");
-          title.textContent = "GAME OVER";
-          message_area.classList.add("warn");
-          message_area.innerHTML = `ヒット数は ${hit_count}<br>ブロー数は ${blow_count}です<br>残念　正解は ${truth_number} でした`
-          input_area.classList.add("invisible");
-          start_area.classList.remove("invisible");
-          start_button.classList.add("restart");
-          start_button.textContent = "再挑戦する";
-        } else if (challenge_time === 9) {
-          console.log(challenge_time);
-          message_area.innerHTML = `ヒット数は ${hit_count}<br>ブロー数は ${blow_count}です<br>次が　最後の　回答です`
-        } else {
-          // console.log(challenge_time);
-        }
+        input_area.classList.add("invisible");
+        start_area.classList.remove("invisible")
+        start_button.textContent = "もう一度遊ぶ";
+      } else if (challenge_time === 10) {
+        title.classList.add("warn");
+        title.textContent = "GAME OVER";
+        message_area.classList.add("warn");
+        message_area.innerHTML = `ヒット数は ${hit_count}<br>ブロー数は ${blow_count}です<br>残念　正解は ${truth_number} でした`
+        input_area.classList.add("invisible");
+        start_area.classList.remove("invisible");
+        start_button.classList.add("restart");
+        start_button.textContent = "再挑戦する";
+      } else if (challenge_time === 9) {
+        console.log(challenge_time);
+        message_area.innerHTML = `ヒット数は ${hit_count}<br>ブロー数は ${blow_count}です<br>次が　最後の　回答です`
       }
 
     } else {
       //３桁の数字でなかった場合
-      // console.log("wrong");
-
       message_area.classList.add("warn");
       message_area.innerHTML = "3ケタの数字を　入力してください";
     }
   });
-
-    //ゲームスタート
-    message_area.innerHTML = "ようこそ　挑戦者さま<br>0から9の　数字の中から<br>わたしが選んだ　３ケタの数字を<br>見事に　当ててみてください<br>回答できる　チャンスは　10回までです。"
-    // console.log(message_area.textContent);
 }
